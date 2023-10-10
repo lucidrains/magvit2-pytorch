fb4c41d37560736544472e445eb9e89efdc88575
Phil Wang <lucidrains@gmail.com>
2023-10-11 01:44:44 +0200

    include latest vq library with lookup free quantization

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'magvit2-pytorch',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'MagViT2 - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/magvit2-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformer',
+    'attention mechanisms',
+    'generative video model'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'vector-quantize-pytorch>=1.9.0',
+    'torch'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
